b5026ab79ca6084a727fcd6017b6c6a803e54b57
nmoosa046 <nmoosa@student.wethinkcode.co.za>
2020-06-22 07:47:23 +0200

    done

--- ex00/ft_print_alphabet.c
+++ ex00/ft_print_alphabet.c
@@ -9,10 +9,15 @@
 /*   Updated: 2020/06/21 10:07:44 by nmoosa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-int ft_putchar(char c);
-void ft_print_alphabet(void);
+int	ft_putchar(char c);
+void	ft_print_alphabet(void);
 
-void ft_print_alphabet(void){
+int	ft_putchar(char c){
+	write(1, &c, 1);
+	return(0);
+}
+
+void	ft_print_alphabet(void){
 	int a;
 
 	a = 'a';
