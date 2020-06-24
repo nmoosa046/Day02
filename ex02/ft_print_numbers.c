c388d1e9d6a4e88c6a2c6782379e748c9c0bf685
Niyaaz Moosa <nmoosa@c4r7s8.wethinkcode.co.za>
2020-06-24 10:12:23 +0200

    done

--- ex02/ft_print_numbers.c
+++ ex02/ft_print_numbers.c
@@ -6,12 +6,11 @@
 /*   By: nmoosa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/21 10:19:49 by nmoosa            #+#    #+#             */
-/*   Updated: 2020/06/21 10:22:38 by nmoosa           ###   ########.fr       */
+/*   Updated: 2020/06/24 10:11:11 by nmoosa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_putchar(char c);
-void	ft_print_numbers(void);
 
 void	ft_print_numbers(void){
 	int num;
