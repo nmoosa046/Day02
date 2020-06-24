76583d0479c92f1ec534800251eeec5eff7cda4c
Niyaaz Moosa <nmoosa@c4r7s8.wethinkcode.co.za>
2020-06-24 10:13:31 +0200

    done

--- ex03/ft_is_negative.c
+++ ex03/ft_is_negative.c
@@ -6,12 +6,11 @@
 /*   By: nmoosa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/21 10:23:57 by nmoosa            #+#    #+#             */
-/*   Updated: 2020/06/21 10:28:04 by nmoosa           ###   ########.fr       */
+/*   Updated: 2020/06/24 10:13:09 by nmoosa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_putchar(char c);
-void	ft_is_negative(int n);
 
 void	ft_is_negative(int n)
 {
